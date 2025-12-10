4b481d2f7d697dc882c091b0ba2b4dc00c4c88e3
darsheel-sharma <darsheelsharma528@gmail.com>
2025-12-10 18:03:21 +0530

    initial commit

---
+++ frontend/components/Header.tsx
@@ -0,0 +1,27 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import NavItems from "./NavItems";
+import UserDropdown from "./UserDropdown";
+
+export default function Header() {
+  return (
+    <header className="sticky top-0 header">
+      <div className="container header-wrapper">
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="Signalist logo"
+            width={140}
+            height={32}
+            className="h-8 w-auto cursor-pointer"
+          ></Image>
+        </Link>
+        <nav className="hidden sm:block">
+          <NavItems />
+        </nav>
+        <UserDropdown />
+      </div>
+    </header>
+  );
+}
---
+++ frontend/global.d.ts
@@ -0,0 +1,11 @@
+// TypeScript declaration for importing CSS/Sass files
+// Place this file in the project root (or `src/`) and ensure it's included by tsconfig.json
+
+declare module '*.css';
+declare module '*.module.css';
+
+declare module '*.scss';
+declare module '*.module.scss';
+
+declare module '*.sass';
+declare module '*.module.sass';
